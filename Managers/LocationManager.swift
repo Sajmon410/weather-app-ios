836ed6e2f80c8136f0e35a6996e60e3eaf11f2ea
Simon Radosavljević <simon.fhz@gmail.com>
2025-02-04 22:56:50 +0100

    Initial commit

---
+++ Managers/LocationManager.swift
@@ -0,0 +1,16 @@
+//
+//  LocationManager.swift
+//  WeatherApp
+//
+//  Created by Simon Radosavljević on 4.2.25..
+//
+
+import Foundation
+import CoreLocation //GPS
+
+class LocationManager NSObject, ObservedObject, CLLocationManagerDelegate {
+    let manager=CLLocationManager()
+
+}
+
+
